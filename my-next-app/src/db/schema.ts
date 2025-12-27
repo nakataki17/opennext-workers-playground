a6d1e4d88a0ce1d8157cb2a711fec0d76ae9f34c
nakataki <me@fusma.net>
2025-12-27 13:04:13 +0900

    setup drizzle schem

--- my-next-app/src/db/schema.ts
+++ my-next-app/src/db/schema.ts
@@ -1,4 +1,4 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const fooTable = sqliteTable("foo", {
   bar: text("bar").notNull().default("Hey!"),
