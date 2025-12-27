c9973c31be2adecc103f1c782e0d332b4ad15285
nakataki <me@fusma.net>
2025-12-27 13:13:30 +0900

    lint

--- my-next-app/drizzle.config.ts
+++ my-next-app/drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: "./db/schema.ts",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL || "",
-    authToken: process.env.TURSO_AUTH_TOKEN || "",
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
